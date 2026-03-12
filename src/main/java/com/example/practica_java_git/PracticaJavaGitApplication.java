5f006d0af958cdadbf1715fa8d882b8990c2c119
MarcoCortes17 <marcocortesmd@gmail.com>
2026-03-11 17:33:10 -0700

    Integracion del repositorio local

---
+++ src/main/java/com/example/practica_java_git/PracticaJavaGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.practica_java_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaJavaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaJavaGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practica_java_git/PracticaJavaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practica_java_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaJavaGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
